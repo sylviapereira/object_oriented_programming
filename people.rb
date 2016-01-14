abe220e6592b3c94e06b072b8320bae9af704d60
Your Name <sylviapereira@gmail.com>
2016-01-14 14:36:59 -0500

    Figured out parent class

--- people.rb
+++ people.rb
@@ -1,19 +1,28 @@
 class Person
-  attr_accessor :name
+  attr_accessor = :person_name
 
-  def initialize(name)
-    @name = name
+  def initialize(person_name)
+    @person_name = person_name
+  end
+
+  def greeting(person_name)
+   puts "Hi, my name is #{@person_name}"
   end
 end
 
-class Student
+class Student < Person
   def learn
     puts "I get it!"
   end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     puts "Everything in Ruby is an Object."
   end
 end
+
+chris = Instructor.new("Chris")
+
+chris.greeting(@person_name)
+chris.teach
