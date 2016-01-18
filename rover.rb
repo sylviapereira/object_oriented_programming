e7b7c31d7ade42abdd46098614a4c691cfba9ece
Your Name <sylviapereira@gmail.com>
2016-01-18 16:21:04 -0500

    Rovers moving, printing final co-ordinates

--- rover.rb
+++ rover.rb
@@ -58,21 +58,32 @@ class Rover
       else chr == "M"
         @x_co -= 1
       end
-
     end
-    print "#{@x_co} #{@y_co} #{@direction}"
   end
+
+def final_co
+  puts "Final position: #{@x_co} #{@y_co} #{@direction}"
+end
+
 end
 
 puts "Plateau size: (x y)"
 plateau_size = gets.chomp.split(" ")
 
-#get_start
 puts "Rover 1 initial start position: (x y d)"
 rover1_start = gets.chomp.split(" ")
 rover1 = Rover.new(rover1_start[0], rover1_start[1], rover1_start[2])
 puts "Rover 1 start position: x:#{rover1_start[0].to_s} y:#{rover1_start[1].to_s} d:#{rover1_start[2].to_s.upcase}"
-
 puts "Rover 1 instructions:"
   rover_instructions = gets.chomp.upcase.split("")
     rover_instructions.each{ |chr| rover1.read_instruction(chr) }
+rover1.final_co
+
+puts "Rover 2 initial start position: (x y d)"
+rover2_start = gets.chomp.split(" ")
+rover2 = Rover.new(rover2_start[0], rover2_start[1], rover2_start[2])
+puts "Rover 2 start position: x:#{rover2_start[0].to_s} y:#{rover2_start[1].to_s} d:#{rover2_start[2].to_s.upcase}"
+puts "Rover 2 instructions:"
+  rover_instructions = gets.chomp.upcase.split("")
+    rover_instructions.each{ |chr| rover2.read_instruction(chr) }
+rover2.final_co
