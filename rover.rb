3e60ec9afc93f58cd2dc0b657729a9ed6f7544a1
Your Name <sylviapereira@gmail.com>
2016-01-18 15:42:57 -0500

    Rover moves, prints out too many co-ordinates

--- rover.rb
+++ rover.rb
@@ -2,76 +2,77 @@ class Rover
   attr_accessor = :x_co, :y_co, :direction
 
   def initialize(x_co, y_co, direction)
-    @x_co = x_co
-    @y_co = y_co
-    @direction = direction
+    @x_co = x_co.to_i
+    @y_co = y_co.to_i
+    @direction = direction.to_s.upcase
   end
 
-  puts "Plateau size: (x y)"
-  plateau_size = gets.chomp.split(" ")
+#  def get_start
+#    puts "Rover initial start position: (x y N/E/S/W)"
+#    x, y, d = gets.chomp.split(" ")
+#  end
 
-  puts "Rover 1 initial start position: (x y N/E/S/W)"
-  x, y, d = gets.chomp.split(" ")
 
-  puts "Rover 1 instructions:"
-  rover1_instructions = gets.chomp
-  rover1_instructions.each! do |read| #or map?
-    read.read_instruction
-  end
 
-  puts "Rover 2 initial start position: (x y N/E/S/W)"
-  x2, y2, d2 = rover2_init = gets.chomp.split(" ")
-
-  puts "Rover 2 instructions:"
-  rover2_instructions = gets.chomp.split(" ")
-
-  def read_instruction
-    if d == "N"
-      if l
-        x_co - 1,
-        d == W
-      elsif r
-        x_co + 1,
-        d = E
-      else m
-        y + 1
+  def read_instruction(chr)
+    if @direction == "N"
+      if chr == "L"
+#        @x_co -= 1
+        @direction = "W"
+      elsif chr == "R"
+  #      @x_co += 1
+        @direction = "E"
+      else chr == "M"
+        @y_co += 1
       end
 
-    elsif d == "E"
-      if l
-        y + 1,
-        N
-      elsif r
-        y - 1
-        S
-      elsif m
-        x + 1
+    elsif @direction == "E"
+      if chr == "L"
+#        @y_co += 1
+        @direction = "N"
+      elsif chr == "R"
+#        @y_co -= 1
+        @direction = "S"
+      else chr == "M"
+        @x_co += 1
       end
 
-    elsif d == "S"
-      if l
-        x + 1
-        E
-      else r
-        x - 1
-        W
-      elsif m
-        y - 1
+    elsif @direction == "S"
+      if chr == "L"
+#        @x_co += 1
+        @direction = "E"
+      elsif chr == "R"
+#        @x_co -= 1
+        @direction = "W"
+      else chr == "M"
+        @y_co -= 1
       end
 
-    elsif d == "W"
-      if l
-        y - 1
-        S
-      else r
-        y + 1
-        N
-      elsif m
-        x - 1
+    else @direction == "W"
+      if chr == "L"
+#        @y_co -= 1
+        @direction = "S"
+      elsif chr == "R"
+#      @y_co += 1
+        @direction = "N"
+      else chr == "M"
+        @x_co -= 1
       end
-
 
     end
-
-
+    print "#{@x_co} #{@y_co} #{@direction}"
+  end
 end
+
+puts "Plateau size: (x y)"
+plateau_size = gets.chomp.split(" ")
+
+#get_start
+puts "Rover 1 initial start position: (x y d)"
+rover1_start = gets.chomp.split(" ")
+rover1 = Rover.new(rover1_start[0], rover1_start[1], rover1_start[2])
+puts "Rover 1 start position: x:#{rover1_start[0].to_s} y:#{rover1_start[1].to_s} d:#{rover1_start[2].to_s.upcase}"
+
+puts "Rover 1 instructions:"
+  rover_instructions = gets.chomp.upcase.split("")
+    rover_instructions.each{ |chr| rover1.read_instruction(chr) }
